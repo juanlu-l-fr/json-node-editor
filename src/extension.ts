9f3be9cccfcf47a9c2914f9669e7e3f7460d9567
juanluislopezfranco <juanluis.lopez@w2m.com>
2024-08-18 11:27:29 +0200

    fix editor bug

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { remove, create, sort, select } from './command.facade';
-import { EditorHandler } from './EditorHandler';
-
+import { CommandFacade } from './command.facade';
 
 
 // This method is called when your extension is activated
@@ -11,12 +9,12 @@ import { EditorHandler } from './EditorHandler';
 export function activate(context: vscode.ExtensionContext) {
 
 	if (vscode.window.activeTextEditor) {
-		const config = vscode.workspace.getConfiguration('json-node-editor.config');
-		const editorHandler: EditorHandler = new EditorHandler(vscode.window.activeTextEditor, config);
-		const removeCmd = vscode.commands.registerCommand('json-node-editor.removeNode', () => remove(editorHandler));
-		const createCmd = vscode.commands.registerCommand('json-node-editor.upsertNode', () => create(editorHandler));
-		const sortCmd = vscode.commands.registerCommand('json-node-editor.sortKeys', () => sort(editorHandler));
-		const selectCmd = vscode.commands.registerCommand('json-node-editor.select', () => select(editorHandler));
+		const facade = new CommandFacade();
+
+		const removeCmd = vscode.commands.registerCommand('json-node-editor.removeNode', () => facade.remove());
+		const createCmd = vscode.commands.registerCommand('json-node-editor.upsertNode', () => facade.create());
+		const sortCmd = vscode.commands.registerCommand('json-node-editor.sortKeys', () => facade.sort());
+		const selectCmd = vscode.commands.registerCommand('json-node-editor.select', () => facade.select());
 		context.subscriptions.push(removeCmd, createCmd, sortCmd, selectCmd);
 	}
 
